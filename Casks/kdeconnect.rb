028c96658c77c6403be3299043efbd47ddffe3d8
mfonville <maarten.fonville@gmail.com>
2025-07-07 07:39:52 +0200

    kdeconnect 5174

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -1,9 +1,9 @@
 cask "kdeconnect" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "5172"
-  sha256  arm:   "c7ef0833a601cce64caff5d4acd8e835652b3e594d5e1f9ec79c71b0cc59088b",
-          intel: "5cd6c820a8ad0eb7edecf6410b0b918aa0c90afb41f916fb970412ca887c6563"
+  version "5174"
+  sha256  arm:   "4215abba55eb715a4cd101b423977fbf29406820837b7b2fb6882902de45864d",
+          intel: "c053f23450f277413d64cadd65e6b24277489e1e029b44330b48492457dfb938"
 
   url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
   name "KDE Connect"
